06cb024f96a966fc6c402cd79d76e09c77b8a60d
wshedden <will.sh@protonmail.com>
2021-07-16 06:43:42 +0100

    Image capture

---
+++ swiper.py
@@ -0,0 +1,74 @@
+import win32gui
+import win32ui
+from ctypes import windll
+from PIL import Image
+import re
+
+class WindowMgr:
+    """Encapsulates some calls to the winapi for window management"""
+
+    def __init__ (self):
+        """Constructor"""
+        self._handle = None
+
+    def find_window(self, class_name, window_name=None):
+        """find a window by its class_name"""
+        self._handle = win32gui.FindWindow(class_name, window_name)
+
+    def _window_enum_callback(self, hwnd, wildcard):
+        """Pass to win32gui.EnumWindows() to check all the opened windows"""
+        if re.match(wildcard, str(win32gui.GetWindowText(hwnd))) is not None:
+            self._handle = hwnd
+
+    def find_window_wildcard(self, wildcard):
+        """find a window whose title matches the wildcard regex"""
+        self._handle = None
+        win32gui.EnumWindows(self._window_enum_callback, wildcard)
+
+    def set_foreground(self):
+        """put the window in the foreground"""
+        win32gui.SetForegroundWindow(self._handle)
+
+hwnd = win32gui.FindWindow(None, 'shella - Discord')
+
+w = WindowMgr()
+w.find_window_wildcard(".*Discord.*")
+w.set_foreground()
+# Change the line below depending on whether you want the whole window
+# or just the client area. 
+#left, top, right, bot = win32gui.GetClientRect(hwnd)
+left, top, right, bottom = win32gui.GetWindowRect(hwnd)
+width = right - left
+height = bottom - top
+
+hwndDC = win32gui.GetWindowDC(hwnd)
+mfcDC  = win32ui.CreateDCFromHandle(hwndDC)
+saveDC = mfcDC.CreateCompatibleDC()
+
+saveBitMap = win32ui.CreateBitmap()
+saveBitMap.CreateCompatibleBitmap(mfcDC, width, height)
+
+saveDC.SelectObject(saveBitMap)
+
+# Change the line below depending on whether you want the whole window
+# or just the client area. 
+#result = windll.user32.PrintWindow(hwnd, saveDC.GetSafeHdc(), 1)
+result = windll.user32.PrintWindow(hwnd, saveDC.GetSafeHdc(), 3)
+print(result)
+
+bmpinfo = saveBitMap.GetInfo()
+bmpstr = saveBitMap.GetBitmapBits(True)
+
+im = Image.frombuffer(
+    'RGB',
+    (bmpinfo['bmWidth'], bmpinfo['bmHeight']),
+    bmpstr, 'raw', 'BGRX', 0, 1)
+
+win32gui.DeleteObject(saveBitMap.GetHandle())
+saveDC.DeleteDC()
+mfcDC.DeleteDC()
+win32gui.ReleaseDC(hwnd, hwndDC)
+
+if result == 1:
+    #PrintWindow Succeeded
+    im.save("test.png")
