d592ccb7e457dac0a0702c964fa21bfc14e0445f
wshedden <will.sh@protonmail.com>
2021-07-21 17:54:03 +0100

    Semi-consistent detection

--- swiper.py
+++ swiper.py
@@ -11,10 +11,11 @@ import numpy as np
 from pynput.keyboard import Key, Controller
 import random
 
+
 class WindowMgr:
     """Encapsulates some calls to the winapi for window management"""
 
-    def __init__ (self):
+    def __init__(self):
         """Constructor"""
         self._handle = None
 
@@ -36,6 +37,9 @@ class WindowMgr:
         """put the window in the foreground"""
         win32gui.SetForegroundWindow(self._handle)
 
+    def set_focus(self):
+        win32gui.SetFocus(self._handle)
+
 
 def save_image(hwnd):
     left, top, right, bottom = win32gui.GetWindowRect(hwnd)
@@ -43,7 +47,7 @@ def save_image(hwnd):
     height = bottom - top
 
     hwndDC = win32gui.GetWindowDC(hwnd)
-    mfcDC  = win32ui.CreateDCFromHandle(hwndDC)
+    mfcDC = win32ui.CreateDCFromHandle(hwndDC)
     saveDC = mfcDC.CreateCompatibleDC()
 
     saveBitMap = win32ui.CreateBitmap()
@@ -69,6 +73,7 @@ def save_image(hwnd):
     if result == 1:
         return im
 
+
 def type_string(kb, s, press_enter):
     for i in s:
         kb.press(i)
@@ -78,88 +83,158 @@ def type_string(kb, s, press_enter):
         kb.release(Key.enter)
 
 
+blur_kernel = np.ones((10, 10), np.float32)/100
+light_blur_kernel = np.ones((5, 5), np.float32)/25
+erode_kernel = np.ones((3, 3), np.uint8)
 
-def filter_image(img):
+
+def scale_image(img, scale_percent):
+    width = int(img.shape[1] * scale_percent / 100)
+    height = int(img.shape[0] * scale_percent / 100)
+    dim = (width, height)
+    img = cv2.resize(img, dim)
+    img = cv2.filter2D(img, -1, blur_kernel)
     return img
+
+def replace_characters(l):
+    for i in range(len(l)):
+        l[i].replace("?", "7")
+        l[i].replace("l", "I")
+
+    return l
+
+
+def set_focus(w):
+    # w.set_foreground()
+    pass
 
 
 pytesseract.pytesseract.tesseract_cmd = r'D:\\Programs\\Tesseract\\tesseract.exe'
-
-
-
-img = cv2.imread("broken9466.png")
-kernel = np.ones((2, 2),np.float32)/4
-dst = cv2.filter2D(img,-1,kernel)
-cv2.imshow("test", dst)
-cv2.waitKey()
-cv2.destroyAllWindows()
-text = pytesseract.image_to_string(img)
-print(text)
-
-input()
-
-
-
-
 windows = Desktop(backend="uia").windows()
-discord_title = [w.window_text() for w in windows if "Discord" in w.window_text()][0]
+discord_title = [w.window_text()
+                 for w in windows if "Discord" in w.window_text()][0]
 hwnd = win32gui.FindWindow(None, discord_title)
 w = WindowMgr()
 w.find_window_wildcard(discord_title)
 keyboard = Controller()
 
 
+lower = np.array([200, 200, 200], dtype="uint16")
+upper = np.array([255, 255, 255], dtype="uint16")
+
+
+
+def test():
+    img = cv2.imread("ex1.png")
+    img2 = cv2.imread("ex2.png")
+    
+    # erode
+    
+    img = cv2.erode(img, np.ones((4, 4), np.uint8))
+    img = cv2.filter2D(img, -1, light_blur_kernel)
+
+    img2 = cv2.erode(img2, np.ones((4, 4), np.uint8))
+    img2 = cv2.filter2D(img2, -1, light_blur_kernel)
+
+    text = pytesseract.image_to_string(img)
+    text2 = pytesseract.image_to_string(img2)
+    print(text, text2)
+
+    cv2.imshow("test", img)
+    cv2.imshow("test", img2)
+
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
+
+
+test()
+input()
+
+
+
+# set_focus(w)
+# time.sleep(2)
+# set_focus(w)
+
+# print("done")
+# input()
+
 for i in range(20000):
     img = save_image(hwnd)
-    # img = cv2.imread("test.png")
     text = pytesseract.image_to_string(img)
     if "to claim it!" in text:
-        print("MATCH FOUND")
-        # Filter image
-        # img = filter_image(img)
-        img_array = np.asarray(img)
-        img_rgb = cv2.cvtColor(img_array, cv2.COLOR_BGR2RGB)
-        # crop_img = img_rgb[100:990, 350:720]
-        crop_img = img_rgb[850:990, 350:720]
+        count = 0
+        while True:
+            print("MATCH FOUND")
 
-        lower = np.array([180, 180, 180], dtype = "uint16")
-        upper = np.array([255, 255, 255], dtype = "uint16")
+            # cv2.imwrite("example_raw.png", np.asarray(img))
+            # time.sleep(0.1)
+            # cv2.imwrite("example_raw2.png", np.asarray(img))
+            # time.sleep(0.1)
+            # cv2.imwrite("example_raw3.png", np.asarray(img))
+            # time.sleep(0.1)
 
-        filtered_image = cv2.inRange(crop_img, lower, upper)
+            # exit
 
-        # cv2.imshow("IMAGE", filtered_image)
-        # cv2.waitKey()
-        # cv2.destroyAllWindows()
-        # text = pytesseract.image_to_string(crop_img)
-        # print(text)
-        # input()
-        text2 = pytesseract.image_to_string(filtered_image)
+            img_array = np.asarray(img)
+            # img_rgb = cv2.cvtColor(img_array, cv2.COLOR_BGR2RGB)
+            # not using RGB
+            crop_img = img_array[700:990, 450:620]
 
-        matches = re.findall(r"\b\w{4}\b", text2)
-        
-        if len(matches) > 0:
-            print(matches)
-            code = matches[-1].lower()
-            print(code)
-            type_string(keyboard, "!claim " + code, True)
-            cv2.imwrite(code + ".png", filtered_image)
+            #scale, erode twice and blur
+            scaled_image = scale_image(crop_img, 500)
+            
+            # scaled_image = scale_image(scaled_image, 200)
 
-        else:
-            print("NO CODES FOUND")
-            cv2.imwrite("broken" + str(random.randint(1, 10000)) + ".png", filtered_image)
+            # scaled_image = cv2.erode(scaled_image, np.ones((3, 3), np.uint8))
+            # scaled_image = cv2.filter2D(scaled_image, -1, light_blur_kernel)
 
-            text = pytesseract.image_to_string(crop_img)
-            matches = re.findall(r"\b\w{4}\b", text)
+            
 
-        
-        print(text2)
+            filtered_image = cv2.inRange(scaled_image, lower, upper)
+            eroded_image = cv2.erode(filtered_image, erode_kernel)
 
-        # cv2.imshow("IMAGE", filtered_image)
-        # cv2.waitKey()
-        # cv2.destroyAllWindows()
 
-        time.sleep(60)
+            text2 = pytesseract.image_to_string(filtered_image)
+
+            matches = re.findall(r"\b\w{4}\b", text2)
+            matches = replace_characters(matches)
+
+            if len(matches) > 0:
+                set_focus(w)
+                code = matches[-1].lower()
+                type_string(keyboard, "!claim " + code, True)
+                print(matches)
+                print(matches[-1])
+                cv2.imwrite(code + ".png", filtered_image)
+                break
+
+            else:
+                print("NO CODES FOUND")
+                n = str(random.randint(1, 10000))
+                cv2.imwrite("broken" + n + str(count) + 
+                            ".png", filtered_image)
+                cv2.imwrite("broken_scaled" + n + str(count) + 
+                            ".png", scaled_image)
+
+                text = pytesseract.image_to_string(crop_img)
+                matches = re.findall(r"\b\w{4}\b", text)
+                img = save_image(hwnd)
+                text = pytesseract.image_to_string(img)
+
+                count += 1
+                time.sleep(0.05)
+                if count > 1:
+                    break
+                print(text2)
+        print("--------------------")
+        time.sleep(150)
 
     time.sleep(0.1)
 
     # w.set_foreground()
+# TODO: refactor
+# TODO: have successful/unsuccessful go in different folders with transcript etc
+# TODO: remove spaces
+# TODO: add automatic view switching
