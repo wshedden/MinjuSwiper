12e59617b11b0766047a7ebcea099f15d3752c85
wshedden <will.sh@protonmail.com>
2021-07-27 03:19:40 +0100

    Pre-refactor

--- swiper.py
+++ swiper.py
@@ -6,35 +6,31 @@ import re
 import time
 import cv2
 import pytesseract
-from pywinauto import Desktop
+import pywinauto
 import numpy as np
 from pynput.keyboard import Key, Controller
-import random
+from random import randint as rand
+import win32com.client
+import os
+
 
 
 class WindowMgr:
-    """Encapsulates some calls to the winapi for window management"""
-
     def __init__(self):
-        """Constructor"""
         self._handle = None
 
     def find_window(self, class_name, window_name=None):
-        """find a window by its class_name"""
         self._handle = win32gui.FindWindow(class_name, window_name)
 
     def _window_enum_callback(self, hwnd, wildcard):
-        """Pass to win32gui.EnumWindows() to check all the opened windows"""
         if re.match(wildcard, str(win32gui.GetWindowText(hwnd))) is not None:
             self._handle = hwnd
 
     def find_window_wildcard(self, wildcard):
-        """find a window whose title matches the wildcard regex"""
         self._handle = None
         win32gui.EnumWindows(self._window_enum_callback, wildcard)
 
     def set_foreground(self):
-        """put the window in the foreground"""
         win32gui.SetForegroundWindow(self._handle)
 
     def set_focus(self):
@@ -83,10 +79,35 @@ def type_string(kb, s, press_enter):
         kb.release(Key.enter)
 
 
+pytesseract.pytesseract.tesseract_cmd = r'D:\\Programs\\Tesseract\\tesseract.exe'
+windows = pywinauto.Desktop(backend="uia").windows()
+discord_title = [w.window_text()
+                 for w in windows if "Discord" in w.window_text()][0]
+hwnd = win32gui.FindWindow(None, discord_title)
+w = WindowMgr()
+w.find_window_wildcard(discord_title)
+keyboard = Controller()
+shell = win32com.client.Dispatch("WScript.Shell")
+
+
+lower = np.array([200, 200, 200], dtype="uint16")
+upper = np.array([255, 255, 255], dtype="uint16")
+
+
+def get_text(img):
+    img_array = np.asarray(img)
+    cropped_img = img_array[200:990, 450:620]
+    scaled_img = scale_image(cropped_img, 1000)
+    black_white_img = cv2.inRange(scaled_img, lower, upper)
+    eroded_img = cv2.erode(black_white_img, np.ones((4, 4), np.uint8))
+    filtered_img = cv2.filter2D(eroded_img, -1, light_blur_kernel)
+    text = pytesseract.image_to_string(filtered_img)
+    return text, filtered_img
+    
+
 blur_kernel = np.ones((10, 10), np.float32)/100
 light_blur_kernel = np.ones((5, 5), np.float32)/25
 erode_kernel = np.ones((3, 3), np.uint8)
-
 
 def scale_image(img, scale_percent):
     width = int(img.shape[1] * scale_percent / 100)
@@ -98,29 +119,19 @@ def scale_image(img, scale_percent):
 
 def replace_characters(l):
     for i in range(len(l)):
-        l[i].replace("?", "7")
-        l[i].replace("l", "I")
-
+        l[i] = l[i].replace("?", "7")
+        l[i] = l[i].replace("l", "I")
+        
+        #TODO: this can be removed if it creates problems:
+        # l[i] = l[i].replace("O", "0") 
+        # l[i] = l[i].replace("0", "O")
     return l
 
 
-def set_focus(w):
-    # w.set_foreground()
-    pass
-
-
-pytesseract.pytesseract.tesseract_cmd = r'D:\\Programs\\Tesseract\\tesseract.exe'
-windows = Desktop(backend="uia").windows()
-discord_title = [w.window_text()
-                 for w in windows if "Discord" in w.window_text()][0]
-hwnd = win32gui.FindWindow(None, discord_title)
-w = WindowMgr()
-w.find_window_wildcard(discord_title)
-keyboard = Controller()
-
-
-lower = np.array([200, 200, 200], dtype="uint16")
-upper = np.array([255, 255, 255], dtype="uint16")
+def set_focus(hwnd):
+    shell.SendKeys('%') 
+    win32gui.SetForegroundWindow(hwnd)
+    
 
 
 
@@ -146,20 +157,6 @@ def test():
     cv2.waitKey()
     cv2.destroyAllWindows()
 
-
-
-test()
-input()
-
-
-
-# set_focus(w)
-# time.sleep(2)
-# set_focus(w)
-
-# print("done")
-# input()
-
 for i in range(20000):
     img = save_image(hwnd)
     text = pytesseract.image_to_string(img)
@@ -167,74 +164,40 @@ for i in range(20000):
         count = 0
         while True:
             print("MATCH FOUND")
-
-            # cv2.imwrite("example_raw.png", np.asarray(img))
-            # time.sleep(0.1)
-            # cv2.imwrite("example_raw2.png", np.asarray(img))
-            # time.sleep(0.1)
-            # cv2.imwrite("example_raw3.png", np.asarray(img))
-            # time.sleep(0.1)
-
-            # exit
-
-            img_array = np.asarray(img)
-            # img_rgb = cv2.cvtColor(img_array, cv2.COLOR_BGR2RGB)
-            # not using RGB
-            crop_img = img_array[700:990, 450:620]
-
-            #scale, erode twice and blur
-            scaled_image = scale_image(crop_img, 500)
-            
-            # scaled_image = scale_image(scaled_image, 200)
-
-            # scaled_image = cv2.erode(scaled_image, np.ones((3, 3), np.uint8))
-            # scaled_image = cv2.filter2D(scaled_image, -1, light_blur_kernel)
-
-            
-
-            filtered_image = cv2.inRange(scaled_image, lower, upper)
-            eroded_image = cv2.erode(filtered_image, erode_kernel)
-
-
-            text2 = pytesseract.image_to_string(filtered_image)
-
-            matches = re.findall(r"\b\w{4}\b", text2)
+            text, final_img = get_text(img)
+            matches = re.findall(r"\b\w{4}\b", text)
             matches = replace_characters(matches)
 
             if len(matches) > 0:
-                set_focus(w)
+                set_focus(hwnd)
                 code = matches[-1].lower()
                 type_string(keyboard, "!claim " + code, True)
                 print(matches)
-                print(matches[-1])
-                cv2.imwrite(code + ".png", filtered_image)
+                cv2.imwrite(code + ".png" , final_img)
                 break
 
             else:
                 print("NO CODES FOUND")
-                n = str(random.randint(1, 10000))
-                cv2.imwrite("broken" + n + str(count) + 
-                            ".png", filtered_image)
-                cv2.imwrite("broken_scaled" + n + str(count) + 
-                            ".png", scaled_image)
-
-                text = pytesseract.image_to_string(crop_img)
-                matches = re.findall(r"\b\w{4}\b", text)
+                cv2.imwrite("broken" + str(rand(1, 10000)) + ".png", np.asarray(final_img))
                 img = save_image(hwnd)
-                text = pytesseract.image_to_string(img)
-
                 count += 1
-                time.sleep(0.05)
-                if count > 1:
+                time.sleep(0.02)
+                if count > 3:
                     break
-                print(text2)
+                print(text)
         print("--------------------")
-        time.sleep(150)
+        time.sleep(160)
 
-    time.sleep(0.1)
+    time.sleep(0.05)
 
     # w.set_foreground()
 # TODO: refactor
 # TODO: have successful/unsuccessful go in different folders with transcript etc
 # TODO: remove spaces
 # TODO: add automatic view switching
+
+
+# Problems:
+# Too slow
+# Sometimes doesn't recognise the codes at all
+# 0s and Os, 5s and 9s mixed up (maybe more)
